c037963af65778e68ad06922c384e94a9a297571
feh-franc0 <fernandofv1110@gmail.com>
2023-10-18 01:41:10 -0300

    Refatorando controller de Autenticação

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -7,13 +7,19 @@ import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-re
 import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository'
 import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-answer-attachments-repository'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { StudentsRepository } from '@/domain/forum/application/repositories/students-repository'
+import { PrismaStudentsRepository } from './prisma/repositories/prisma-students-repository'
 
 @Module({
   providers: [
     PrismaService,
     {
       provide: QuestionsRepository,
-      useClass: PrismaQuestionsRepository,
+      useClass: PrismaStudentsRepository,
+    },
+    {
+      provide: StudentsRepository,
+      useClass: PrismaStudentsRepository,
     },
     PrismaQuestionCommentRepository,
     PrismaQuestionAttachmentsRepository,
@@ -24,6 +30,7 @@ import { QuestionsRepository } from '@/domain/forum/application/repositories/que
   exports: [
     PrismaService,
     QuestionsRepository,
+    StudentsRepository,
     PrismaQuestionCommentRepository,
     PrismaQuestionAttachmentsRepository,
     PrismaAnswersRepository,
